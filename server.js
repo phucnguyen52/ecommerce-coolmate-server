652b82af6047a239f4a4e89f35ad9b2369295496
PHUCNGUYEN <phucfe050203@gmail.com>
2025-08-28 22:28:19 +0700

    Update vercel

--- server.js
+++ server.js
@@ -5,34 +5,28 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const corsOptions = {
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
-    credentials: true 
+  origin: ["*"],
+  credentials: true,
 };
 
-
-const {connectToDB} = require("./config/mysql.js");
-const {app, server} = require("./socket/socket");
-
-
+const { connectToDB } = require("./config/mysql.js");
+const { app, server } = require("./socket/socket");
 
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //router
 const customerRouter = require("./router/customer.js");
 const adminRouter = require("./router/admin.js");
 const uploadRouter = require("./router/upImage.js");
 
-
 //api
 app.use("/api/customer", customerRouter);
 app.use("/api/admin", adminRouter);
 app.use("/upload", uploadRouter);
 
-
 server.listen(process.env.PORT, async () => {
-	await connectToDB();
+  await connectToDB();
 });
