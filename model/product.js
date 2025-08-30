ab638c1604fa893ff3f5b5374fd0b6c7b01ab174
PHUCNGUYEN <phucfe050203@gmail.com>
2025-08-30 23:08:12 +0700

    update product

--- model/product.js
+++ model/product.js
@@ -1,42 +1,49 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const {sequelize} = require("../config/mysql");
-const {ProductDetail} = require("./productDetail");
+const { Sequelize, DataTypes } = require("sequelize");
+const { sequelize } = require("../config/mysql");
+const { ProductDetail } = require("./productDetail");
 
-const Product = sequelize.define("Product",
-    {
-        nameProduct: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        descriptionProducts: {
-            type: DataTypes.TEXT,
-        },
-        quantitySell: {
-            type: DataTypes.INTEGER,
-        },
-        price: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        discount: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        image: {
-            type: DataTypes.TEXT,
-        },
-        createDate: {
-            type: DataTypes.DATE,
-            defaultValue: Sequelize.fn("NOW")
-        },
-        brand : DataTypes.TEXT
+const Product = sequelize.define(
+  "Product",
+  {
+    nameProduct: {
+      type: DataTypes.TEXT,
+      allowNull: false,
     },
-    {
-        tableName : "product"
-    }
+    descriptionProducts: {
+      type: DataTypes.TEXT,
+    },
+    quantitySell: {
+      type: DataTypes.INTEGER,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    discount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.TEXT,
+    },
+    createDate: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.fn("NOW"),
+    },
+    brand: DataTypes.TEXT,
+  },
+  {
+    tableName: "product",
+  }
 );
 
-Product.hasMany(ProductDetail);
-ProductDetail.belongsTo(Product);
+Product.hasMany(ProductDetail, {
+  foreignKey: "ProductId",
+  constraints: false,
+});
+ProductDetail.belongsTo(Product, {
+  foreignKey: "ProductId",
+  constraints: false,
+});
 
-module.exports = {Product};
+module.exports = { Product };
